4f4ff067bcd4a8e1032a0a644edbe169a82c92ac
Najeeb Arif <NajeebArif786@outlook.com>
2023-02-04 13:06:59 +0530

    Initial code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Thid is a header section
+      </header>
+      <section>
+        This is another section
+      </section>
+      <section>
+        This is another section.
+      </section>
+    </div>
+  );
+}
+
+export default App;
