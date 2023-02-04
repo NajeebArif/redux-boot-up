55776e0cacdf3448cdbcde6917f6b3b3d9bfc0be
Najeeb Arif <NajeebArif786@outlook.com>
2023-02-04 14:04:08 +0530

    Add legacy redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,37 @@
-import React,{useState} from 'react'
+import React from 'react'
+import {legacy_createStore as createStore} from 'redux'
 
-export default function Counter() {
-    // State: a counter value
-    const [counter, setCounter] = useState(0)
-  
-    // Action: code that causes an update to the state when something happens
-    const increment = () => {
-      setCounter(prevCounter => prevCounter + 1)
-    }
-  
-    // View: the UI definition
+function counterReducer(state={value:0}, action){
+  switch(action.type){
+    case 'INCREMENT':
+      return {value: state.value+1}
+    case 'DECREMENT':
+      return {value: state.value-1}
+    default:
+      return state;
+  }
+}
+
+function Counter() {
+
+  const store = createStore(counterReducer);
+
+  const increment = ()=>{
+    store.dispatch({type: 'INCREMENT'})
+  }
+
+  const decrement = ()=>{
+    store.dispatch({type: 'DECREMENT'})
+  }
+
+  store.subscribe(()=> console.log('Value',store.getState().value))
+
     return (
       <div>
-        Value: {counter} <button onClick={increment}>Increment</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
       </div>
     )
   }
+
+export default Counter;
